02b865de0fa44b3b47dfc98602adf3d633a696c0
LYQ <2089887529@qq.com>
2026-02-06 16:48:09 +0800

    feat: 20260206 添加雷达以及imu后

--- src/aicar_simulation/launch/gazebo.launch.py
+++ src/aicar_simulation/launch/gazebo.launch.py
@@ -54,7 +54,7 @@ def generate_launch_description():
     spawn_entity = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-topic','robot_description', '-entity', 'aicar', '-z', '1.0'],
+        arguments=['-topic','robot_description', '-entity', 'aicar', '-z', '0.1'],
         output='screen'
     )
     ld = LaunchDescription()
